7e6499f7bb561fd2d12e18549915460b37feb5df
trikitrok <trikitrok@gmail.com>
2014-08-09 18:52:49 +0200

    It finds no index for a word containing no capital letters as long as the word is one character long

--- UpperCaseFinderSpec.js
+++ UpperCaseFinderSpec.js
@@ -4,7 +4,11 @@ function findCapitalLetterPositions(word) {
   if(word.length === 0) {
     return [];    
   }
-  return [0];
+
+  if(word[0].toUpperCase() == word[0]) {
+    return [0];
+  }
+  return [];
 }
 
 describe("Upper case letters finder", function() {
@@ -16,5 +20,7 @@ describe("Upper case letters finder", function() {
     expect(findCapitalLetterPositions("A")).toEqual([0]);
   });
 
-
+  it("produces no index for a one letter word containing no capital letters", function() {
+    expect(findCapitalLetterPositions("a")).toEqual([]);
+  });
 });
