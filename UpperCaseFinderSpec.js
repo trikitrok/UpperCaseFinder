0f7447460134f91f36d1cbe805bdc698ca0c0b60
trikitrok <trikitrok@gmail.com>
2014-08-09 18:47:42 +0200

    It finds the index of a word with one capital letter

--- UpperCaseFinderSpec.js
+++ UpperCaseFinderSpec.js
@@ -1,11 +1,19 @@
 'use strict';
 
 function findCapitalLetterPositions(word) {
-  return [];
+  if(word.length === 0) {
+    return [];    
+  }
+  return [0];
 }
 
 describe("Upper case letters finder", function() {
   it("an empty word contains no capital letters", function() {
     expect(findCapitalLetterPositions("")).toEqual([]);
   });
+
+  it("finds the index of a word with one capital letter", function() {
+    expect(findCapitalLetterPositions("A")).toEqual([0]);
+  });
+
 });
