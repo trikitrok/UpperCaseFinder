e65df3204a2010b81b28f058f11b871266082605
trikitrok <trikitrok@gmail.com>
2014-08-09 18:44:28 +0200

    An empty word contains no capital letters

---
+++ UpperCaseFinderSpec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function findCapitalLetterPositions(word) {
+  return [];
+}
+
+describe("Upper case letters finder", function() {
+  it("an empty word contains no capital letters", function() {
+    expect(findCapitalLetterPositions("")).toEqual([]);
+  });
+});
